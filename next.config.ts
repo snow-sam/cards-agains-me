e584f940b5e133a0c31e131e45049083c5c7b75a
Samuel Souza <samuel_f2@icloud.com>
2024-12-02 19:26:33 -0300

    fix: output in next.conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   env: {
     SERVER_HOST: process.env.SERVER_HOST,
     SERVER_PORT: process.env.SERVER_PORT
